dd4282293d0b874ef5bd44816d2f6fda82739ad1
soaicasancho <chichchoehoa001@gmail.com>
2026-01-15 09:58:19 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", function (event) {
-  console.log("✅ Service Worker installed.");
-});
-
-self.addEventListener("fetch", function (event) {
-  // Dành cho cache nếu cần mở rộng trong tương lai
-});
